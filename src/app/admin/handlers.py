c6d87c873480f852a7e7505b40709e50a33f09b0
Georgiy Vasin <georgijvasin01@gmail.com>
2024-10-06 22:43:47 +0300

    create admin handlers

---
+++ src/app/admin/handlers.py
@@ -0,0 +1,35 @@
+import app.utils.keyboards as kb
+import requests
+
+from aiogram import Router, F
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, CallbackQuery
+
+from app.utils.states import AdminStates
+from app.filters import Admin, InChat
+from database import controller
+
+
+router = Router()
+
+@router.message(Admin(), Command('admin_panel'), InChat())
+async def admin_panel(message: Message) -> None:
+    await message.answer('Возможные команды админа:', reply_markup=kb.admin)
+
+
+@router.callback_query(Admin(), F.data == 'mail')
+async def get_admin_message(call: CallbackQuery, state: FSMContext) -> None:
+    await state.set_state(AdminStates.get_msg)
+    await call.message.answer('Пришлите ваше сообщение для рассылки')  # type: ignore
+
+
+@router.message(AdminStates.get_msg)
+async def send_mail(message: Message, state: FSMContext) -> None:
+    await state.clear()
+    await message.answer('Подождите идет рассылка...')
+    
+    async for user in controller.users.get_all():
+        await requests.send_copy(message, chat_id=user.telegram_id)
+    
+    await message.answer('Рассылка завершена', reply_markup=kb.remove)
