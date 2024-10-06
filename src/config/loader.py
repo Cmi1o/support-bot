be340e1717410c6448d7b5464468857210e7f045
Georgiy Vasin <georgijvasin01@gmail.com>
2024-10-06 15:23:38 +0300

    create .env loader

---
+++ src/config/loader.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    bot_token: str
+    admin_id: int
+    database_url: str
+    forum_topic_id: str
+    
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+        case_sensitive=False
+    )
