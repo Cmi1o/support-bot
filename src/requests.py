027eb505bef67969481df76bec9410f61347dbc7
Georgiy Vasin <georgijvasin01@gmail.com>
2024-10-06 22:43:37 +0300

    create telegram async requests

---
+++ src/requests.py
@@ -0,0 +1,17 @@
+import aiohttp
+
+from aiogram.types import Message
+
+
+async def send_copy(message: Message, **data) -> None:
+    try:
+        await message.send_copy(**data)
+    except TypeError: ...
+
+
+async def send_message(bot_token: str, **data) -> None:
+    async with aiohttp.ClientSession() as session:
+        await session.post(
+            url=f'https://api.telegram.org/bot{bot_token}/sendMessage',
+            json=data
+        )
