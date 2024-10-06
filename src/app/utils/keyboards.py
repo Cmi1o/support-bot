515bc09be031e62249a42b83d68fbedf406d7d5e
Georgiy Vasin <georgijvasin01@gmail.com>
2024-10-06 22:39:38 +0300

    create tg keyboards

---
+++ src/app/utils/keyboards.py
@@ -0,0 +1,83 @@
+from aiogram.types import (
+    KeyboardButton,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove, 
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
+
+
+remove = ReplyKeyboardRemove()
+
+registration = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text='Зарегистрироваться', 
+                callback_data='reg'
+            )
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
+
+admin = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text='Сделать рассылку', 
+                callback_data='mail'
+            )
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
+
+support = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text='Поддержка', 
+                callback_data='support'
+            )
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
+
+reference = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text='Перейти на сайт', 
+                url='https://habr.com/ru/companies/otus/articles/736244/'
+            )
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
+
+confirm = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Да'), KeyboardButton(text='Нет')]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
+
+end_dialog = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text='Завершить диалог', 
+                callback_data='end'
+            )
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
