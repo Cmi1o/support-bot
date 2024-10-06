16a053f5363ede6df032915085b1a5c3e0f71cc7
Georgiy Vasin <georgijvasin01@gmail.com>
2024-10-06 23:36:55 +0300

    create main bot config and start

---
+++ src/main.py
@@ -0,0 +1,33 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.exceptions import TelegramNetworkError
+
+from app import middlewares
+from app.dispatcher_builder import *
+from app.users_cleaner import InactiveUsersCleaner
+from config import bot_token, forum_topic_id
+
+
+async def main() -> None:
+    bot = Bot(bot_token, parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+    cleaner = InactiveUsersCleaner(bot_token, forum_topic_id)
+    
+    dp.update.outer_middleware(middlewares.outer.AntiFlood(1))
+    
+    dp.include_router(router)
+    
+    await asyncio.gather(
+        dp.start_polling(bot),
+        cleaner.start_polling()
+    )
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except (TelegramNetworkError, KeyboardInterrupt) as error:
+        if isinstance(error, TelegramNetworkError):
+            print('Network error')
