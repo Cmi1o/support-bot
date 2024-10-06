552cbb2a8fa90b5a184b71a6abbe64c7ff2cee86
Georgiy Vasin <georgijvasin01@gmail.com>
2024-10-06 22:40:02 +0300

    create is admin filter

---
+++ src/app/filters/admin_protection.py
@@ -0,0 +1,9 @@
+from aiogram.types import Message
+from aiogram.filters import Filter
+
+from config import admin_id
+
+
+class Admin(Filter):
+    async def __call__(self, message: Message) -> bool:
+        return message.from_user.id == admin_id  # type: ignore
