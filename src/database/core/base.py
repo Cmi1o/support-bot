b1081bbf6ecf44310c4cfdd36a364d406e48cccc
Georgiy Vasin <georgijvasin01@gmail.com>
2024-10-06 15:17:15 +0300

    create declarative base db table

---
+++ src/database/core/base.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import (
+    mapped_column,
+    DeclarativeBase,
+    Mapped, declared_attr
+)
+from sqlalchemy.ext.asyncio import AsyncAttrs
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f'{cls.__name__.lower()}s'
