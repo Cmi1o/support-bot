def47785724b516200f7895fbab42dc6863c82a3
Georgiy Vasin <georgijvasin01@gmail.com>
2024-10-07 21:16:31 +0300

    add support of photo, video and others in topic message handler

--- src/app/handlers/message.py
+++ src/app/handlers/message.py
@@ -1,14 +1,11 @@
-import app.utils.keyboards as kb
 import requests
 
 from aiogram import Router, F
 from aiogram.types import Message
-from aiogram.fsm.context import FSMContext
 
-from app.filters import InChat, InTopic, FailCommand
+from app.filters import InTopic, FailCommand
 from config import bot_token
 from database import controller
-
 
 
 router = Router()
@@ -18,8 +15,10 @@ async def fail_command_echo(message: Message) -> None:
     await message.answer('Этой команды не существует, попробуйте другую')
 
 
-@router.message(InTopic(), F.text)
-async def topic_echo(message: Message) -> None: 
+@router.message(
+    InTopic(), F.text | F.photo | F.document | F.audio | F.video | F.voice | F.video_note
+)
+async def topic_echo(message: Message) -> None:
     user = await controller.users.get_by(thread_id=message.message_thread_id)
     
     if user and user.telegram_id:
