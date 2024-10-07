ddd2058d60ec80cdf194d7e88a1fb6bf864601b3
Georgiy Vasin <georgijvasin01@gmail.com>
2024-10-07 20:45:07 +0300

    delete Auth filter

--- src/app/filters/__init__.py
+++ src/app/filters/__init__.py
@@ -2,4 +2,3 @@ from .in_topic import InTopic
 from .in_chat import InChat
 from .admin_protection import Admin
 from .fail_command import FailCommand
-from .auth_check import Auth
--- src/app/filters/auth_check.py
+++
@@ -1,9 +0,0 @@
-from aiogram.filters import Filter
-from aiogram.types import CallbackQuery
-
-from database import controller
-
-
-class Auth(Filter):
-    async def __call__(self, call: CallbackQuery) -> bool:
-        return bool(await controller.users.get_by(telegram_id=call.from_user.id))
