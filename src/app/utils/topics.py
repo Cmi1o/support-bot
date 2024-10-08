22cc991ac6c533651d2dfdee30cbff13faaf16cc
Georgiy Vasin <georgijvasin01@gmail.com>
2024-10-08 11:38:33 +0300

    create new func that return base bot interactions url

--- src/app/utils/topics.py
+++ src/app/utils/topics.py
@@ -1,6 +1,10 @@
 from aiohttp import ClientResponse, ClientSession
 
 
+def url(bot_token: str) -> str:
+    return f'https://api.telegram.org/bot{bot_token}'
+
+
 async def create_topic(bot_token: str, forum_id: int | str, name: str) -> ClientResponse:
     '''Create new topic in the forum
     
@@ -18,7 +22,7 @@ async def create_topic(bot_token: str, forum_id: int | str, name: str) -> Client
     '''
     async with ClientSession() as session:
         return await session.post(
-            url=f'https://api.telegram.org/bot{bot_token}/createForumTopic',
+            url=f'{url(bot_token)}/createForumTopic',
             json={
                 'chat_id': forum_id,
                 'name': name
@@ -33,7 +37,7 @@ async def delete_topic(
 ) -> ClientResponse:
     async with ClientSession() as session:
         return await session.post(
-            url=f'https://api.telegram.org/bot{bot_token}/deleteForumTopic',
+            url=f'{url(bot_token)}/deleteForumTopic',
             json={
                 'chat_id': forum_id,
                 'message_thread_id': message_thread_id
