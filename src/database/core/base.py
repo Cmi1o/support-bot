e63df5e964e65577e2d2e334cf5b977d18d2871a
Georgiy Vasin <georgijvasin01@gmail.com>
2025-01-24 22:29:01 +0300

    feat: add timestamps for created_at and updated_at in Base model

--- src/database/core/base.py
+++ src/database/core/base.py
@@ -1,11 +1,16 @@
-from sqlalchemy import BigInteger
+import re
+from datetime import datetime
+
+from sqlalchemy import BigInteger, DateTime
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
 
 
 class Base(AsyncAttrs, DeclarativeBase):
     id: Mapped[int] = mapped_column(BigInteger(), primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(), default=datetime.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime(), default=datetime.now())
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
-        return f'{cls.__name__.lower()}s'
+        return re.sub(r'(?<!^)(?=[A-Z])', '_', cls.__name__).lower() + 's'
