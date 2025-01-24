4528bd24080744b8f431630a12eb14ac05a849a7
Georgiy Vasin <georgijvasin01@gmail.com>
2025-01-24 22:52:59 +0300

    refactor: simplify message handler by using a new type for valid redirect messages types

--- src/app/bot/handlers/message.py
+++ src/app/bot/handlers/message.py
@@ -7,6 +7,9 @@ from config import bot_token
 from database import service
 
 router = Router()
+TRedirectMessage = (
+    F.text | F.photo | F.document | F.audio | F.video | F.voice | F.video_note
+)
 
 
 @router.message(InTopic(), FailCommand())
@@ -14,10 +17,7 @@ async def fail_command_echo(message: Message) -> None:
     await message.answer('Этой команды не существует, попробуйте другую')
 
 
-@router.message(
-    InTopic(),
-    F.text | F.photo | F.document | F.audio | F.video | F.voice | F.video_note,
-)
+@router.message(InTopic(), TRedirectMessage)
 async def topic_echo(message: Message) -> None:
     user = await service.users.get_by(thread_id=message.message_thread_id)
 
